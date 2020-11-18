f9ba5248348a9a55a9f17a34920f5be98102d459
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-18 14:48:53 -0800

    Imports search page, implements router route and switch

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+// Import pages
+import SearchBooks from "./pages/SearchBooks";
+// import SavedBooks from "./pages/SavedBooks";
+
+// Import components
+// NavBar
+// Header
+
+function App() {
+  return (
+    <Router>
+      {/* <NavBar />
+      <Banner /> */}
+      <Switch>
+        <Route exact path={"/"}>
+          <SearchBooks />
+        </Route>
+        {/* <Route exact path={"/saved"}>
+          <SavedBooks />
+        </Route> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
