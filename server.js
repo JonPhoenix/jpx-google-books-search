d9b091d4e0a3de7e78b1070c57e3fba2b8ec1f22
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-17 17:10:53 -0800

    Adding routes both API and view

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
 const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,14 +12,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Add routes, both API and view
+app.use(routes);
 
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 // Start the API server
 app.listen(PORT, () => {
