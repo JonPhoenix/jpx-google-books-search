c49df2d6050c4edc42b4100583d8e2bf10a11536
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-18 15:32:59 -0800

    Updates react strict mode and imports bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 import registerServiceWorker from "./registerServiceWorker";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>,
+document.getElementById("root")
+);
+
+// Setting up app service worker to work offline: register()
 registerServiceWorker();
