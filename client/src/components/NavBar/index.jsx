fa4789fdbde844cb837d5f5379b523ceefaaae13
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-22 14:35:58 -0800

    Updates nav links

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -4,8 +4,8 @@ import "./style.css";
 
 function NavBar() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
+    <>
+      <nav className="nav-item">
         <Link
           to="/"
           className={
@@ -14,32 +14,34 @@ function NavBar() {
         >
           Google Books
         </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/search"
-          className={
-            window.location.pathname === "/search"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Search
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/saved"
-          className={
-            window.location.pathname === "/saved"
-              ? "nav-link active"
-              : "nav-link"
-          }
-        >
-          Saved
-        </Link>
-      </li>
-    </ul>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/saved"
+              className={
+                window.location.pathname === "/saved"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Saved
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
