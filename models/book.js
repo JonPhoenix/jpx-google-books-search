a3af2e00e9ec129b915bb8db6424599b2e40d7d6
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-22 14:28:58 -0800

    Updates model schema

--- models/book.js
+++ models/book.js
@@ -2,36 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-  googleId: {
-    type: String,
-    unique: true,
-    required: true,
+  title: { 
+    type: String, 
+    required: true 
   },
-
-  title: {
-    type: String,
-    trim: true,
+  subtitle: { 
+    type: String 
   },
-
-  authors: {
-    type: [String],
-    trim: true,
+  authors: { 
+    type: [String], 
+    required: true 
   },
-
-  description: {
-    type: String,
-    trim: true,
+  link: { 
+    type: String, 
+    required: true 
   },
-
-  image: {
-    type: String,
-    trim: true,
+  description: { 
+    type: String, 
+    required: true 
   },
-
-  link: {
-    type: String,
-    trim: true,
+  image: { 
+    type: String, 
+    required: true 
   },
+  googleId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  }
 });
 
 const Book = mongoose.model("Book", BookSchema);
