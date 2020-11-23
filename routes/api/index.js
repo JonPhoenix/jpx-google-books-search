93b0a57dd8b711e966b39c091e281a8ff26c8854
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-22 16:24:16 -0800

    Final review

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,9 +9,4 @@ router.use("/books", bookRoutes);
 // Google Routes
 router.use("/google", googleRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
 module.exports = router;
